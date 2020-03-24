f9ca77020957e12a483491a7b8c2975c176a6ed8
Nhan Nguyen <nkmnhan@gmail.com>
2020-03-25 02:17:26 +0700

    Implement i18n feature

---
+++ ResourceManager/Helpers/Pickers.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Forms;
+
+namespace ResourceManager.Helpers
+{
+    public static class Pickers
+    {
+        public static string FolderPicker()
+        {
+            using (var folderBrowserDialog = new FolderBrowserDialog())
+            {
+                DialogResult result = folderBrowserDialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+                {
+                    return folderBrowserDialog.SelectedPath;
+                }
+            }
+            return string.Empty;
+        }
+
+        public static string FilePicker()
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.DefaultExt = ".xlsx";
+                openFileDialog.Filter = "Excel |*.xlsx";
+                DialogResult result = openFileDialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(openFileDialog.FileName))
+                {
+                    return openFileDialog.FileName;
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
--- ResourceManager/ViewModels/I18nViewModel.cs
+++ ResourceManager/ViewModels/I18nViewModel.cs
@@ -1,5 +1,8 @@
 ﻿
 using Caliburn.Micro;
+using ResourceManager.Core.Services;
+using ResourceManager.Helpers;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -7,33 +10,208 @@ namespace ResourceManager.ViewModels
 {
     public class I18nViewModel : Screen
     {
-        public string Name { get; set; }
-        public I18nViewModel()
+        private string errorMsg;
+
+        // Generate
+        private string generateSelectedExcel;
+        private string generateSaveFolder;
+
+        // Export
+        private string exportSourceFolder;
+        private string exportSaveFolder;
+        private string exportExcelName;
+
+        // Update
+        private string updateSelectedExcel;
+        private string updateI18nFolder;
+
+        public string ErrorMsg
         {
-            Name = "I18n";
+            get { return errorMsg; }
+            set
+            {
+                errorMsg = value;
+                NotifyOfPropertyChange(() => errorMsg);
+                ShowErrorMessage();
+            }
+        }
+        public string GenerateSelectedExcel
+        {
+            get { return generateSelectedExcel; }
+            set
+            {
+                generateSelectedExcel = value;
+                NotifyOfPropertyChange(() => generateSelectedExcel);
+            }
+        }
+        public string GenerateSaveFolder
+        {
+            get { return generateSaveFolder; }
+            set
+            {
+                generateSaveFolder = value;
+                NotifyOfPropertyChange(() => generateSaveFolder);
+            }
+        }
+
+        public string ExportSourceFolder
+        {
+            get { return exportSourceFolder; }
+            set
+            {
+                exportSourceFolder = value;
+                NotifyOfPropertyChange(() => exportSourceFolder);
+            }
+        }
+        public string ExportSaveFolder
+        {
+            get { return exportSaveFolder; }
+            set
+            {
+                exportSaveFolder = value;
+                NotifyOfPropertyChange(() => exportSaveFolder);
+            }
+        }
+        public string ExportExcelName
+        {
+            get { return exportExcelName; }
+            set
+            {
+                exportExcelName = value;
+                NotifyOfPropertyChange(() => exportExcelName);
+            }
+        }
+
+        public string UpdateSelectedExcel
+        {
+            get { return updateSelectedExcel; }
+            set
+            {
+                updateSelectedExcel = value;
+                NotifyOfPropertyChange(() => updateSelectedExcel);
+            }
+        }
+        public string UpdateI18nFolder
+        {
+            get { return updateI18nFolder; }
+            set
+            {
+                updateI18nFolder = value;
+                NotifyOfPropertyChange(() => updateI18nFolder);
+            }
+        }
+
+        public void SelectFile(object source, RoutedEventArgs eventArgs)
+        {
+            var button = source as Button;
+            if (button != null && !string.IsNullOrWhiteSpace(button?.Tag?.ToString()))
+            {
+                var propInfo = this.GetType().GetProperty(button.Tag.ToString());
+                var filePath = Pickers.FilePicker();
+                propInfo.SetValue(this, filePath);
+            }
+
         }
 
         public void SelectFolder(object source, RoutedEventArgs eventArgs)
         {
             var button = source as Button;
-            //using (var fbd = new FolderBrowserDialog())
-            //{
-            //    DialogResult result = fbd.ShowDialog();
+            if (button != null && !string.IsNullOrWhiteSpace(button?.Tag?.ToString()))
+            {
+                var propInfo = this.GetType().GetProperty(button.Tag.ToString());
+                var folder = Pickers.FolderPicker();
+                propInfo.SetValue(this, folder);
+            }
 
-            //    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
-            //    {
-            //        string[] files = Directory.GetFiles(fbd.SelectedPath);
+        }
 
-            //        System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
-            //    }
-            //}
+        public async void Generate(object source, RoutedEventArgs eventArgs)
+        {
+            if (!ValidateGenerate())
+            {
+                return;
+            }
 
-            //using (var fbd = new OpenFileDialog())
-            //{
-            //    fbd.DefaultExt = ".xlsx";
-            //    fbd.Filter = "Excel |*.xlsx";
-            //    DialogResult result = fbd.ShowDialog();
-            //}
+            var languageResourece = ExcelService.Read(GenerateSelectedExcel);
+            await I18nService.Create(languageResourece, GenerateSaveFolder);
+        }
+
+        public async void Export(object source, RoutedEventArgs eventArgs)
+        {
+            if (!ValidateExport())
+            {
+                return;
+            }
+
+            var excelData = await I18nService.ConvertToExcelData(exportSourceFolder);
+            var excelPath = Path.Combine(ExportSaveFolder, $"{ExportExcelName}.xlsx");
+            ExcelService.Export(excelData, excelPath);
+        }
+
+        public async void Update(object source, RoutedEventArgs eventArgs)
+        {
+            if (!ValidateUpdate())
+            {
+                return;
+            }
+
+            var languageResourece = ExcelService.Read(UpdateSelectedExcel);
+            await I18nService.Update(UpdateI18nFolder, languageResourece);
+        }
+
+        private bool ValidateGenerate()
+        {
+            if (string.IsNullOrWhiteSpace(GenerateSelectedExcel))
+            {
+                ErrorMsg = "Invalid excel path";
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(GenerateSaveFolder))
+            {
+                ErrorMsg = "Invalid save folder";
+                return false;
+            }
+            return true;
+        }
+
+        private bool ValidateExport()
+        {
+            if (string.IsNullOrWhiteSpace(ExportSourceFolder))
+            {
+                ErrorMsg = "Invalid source folder";
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(ExportSaveFolder))
+            {
+                ErrorMsg = "Invalid save folder";
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(ExportExcelName))
+            {
+                ErrorMsg = "Empty excel name";
+                return false;
+            }
+            return true;
+        }
+
+        private bool ValidateUpdate()
+        {
+            if (string.IsNullOrWhiteSpace(UpdateSelectedExcel))
+            {
+                ErrorMsg = "Invalid excel path";
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(UpdateI18nFolder))
+            {
+                ErrorMsg = "Invalid i18n folder";
+                return false;
+            }
+            return true;
+        }
+
+        private void ShowErrorMessage()
+        {
+            MessageBox.Show($"{errorMsg}", "Error", MessageBoxButton.OK);
         }
     }
 }
